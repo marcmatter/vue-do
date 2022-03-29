6f616e87c34c44a7de8ae93db12a2553cb255619
Marc Matter <marc.matter2004@gmail.com>
2022-03-29 19:36:07 +0200

    finish cypress tests

--- src/tests/cypress/integration/3-manage-categories/manage-categories.spec.js
+++ src/tests/cypress/integration/3-manage-categories/manage-categories.spec.js
@@ -19,4 +19,30 @@ describe('filter entries', () => {
     cy.get('[data-cy=addCategoryBtn]').click();
     cy.get('[data-cy=closeCategoryEditorBtn]').click();
   });
+
+  it('assign entry to new category', () => {
+    cy.get('.todo-item [data-cy=entryCategorySelect]').eq(1).click();
+    cy.get('.todo-item [data-cy=entryCategorySelect]').eq(1).contains('Cypress').click();
+  });
+
+  it('filter new category', () => {
+    cy.get('[data-cy=navCategories] [data-cy=Cypress]').click();
+    cy.get('body').find('.todo-item').should('have.length', 1);
+    cy.get('[data-cy=navCategories] [data-cy="All Categories"]').click();
+  });
+
+  it('check unable to delete category with assigned entry', () => {
+    cy.get('[data-cy=manageCategories]').click();
+    cy.get('[data-cy="deleteCategoryBtn_Cypress"]').click({ force: true });
+    cy.get('[data-cy="deleteCategoryBtn_Cypress"]').realHover();
+    cy.get('.popup').contains("can't delete").should('be.visible');
+    cy.get('[data-cy=closeCategoryEditorBtn]').click();
+  });
+
+  it('delete category', () => {
+    cy.get('.todo-item [data-cy=entryCategorySelect] [data-cy=removeSelectValue]').eq(1).click();
+    cy.get('[data-cy=manageCategories]').click();
+    cy.get('[data-cy="deleteCategoryBtn_Cypress"]').click();
+    cy.get('[data-cy="deleteCategoryBtn_Cypress"]').should('not.exist');
+  });
 });
